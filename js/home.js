3ba5d3d8a9dbf6726f898939575b6b0cd9901e11
Thomas Arsenault <thomasarsenaultj@gmail.com>
2020-06-13 19:27:45 -0400

    i dont even remember lol

--- js/home.js
+++ js/home.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     $.getJSON("../data/home.json", function(data){
         let bio_text = "";
-        let nicknames = data.nicknames.sort(() => Math.random() - 0.5);;
+        let nicknames = data.nicknames.sort(() => Math.random() - 0.5);
         let bio_split = data.bio.split("@nickname@")
         bio_text += bio_split[0];
         for(let i = 1; i < bio_split.length; i++){
@@ -10,6 +10,7 @@ $(document).ready(function() {
             bio_text += bio_split[i];
         }
 
-        $("#bio-placeholder").html(bio_text.toUpperCase());
+        console.log(bio_text);
+        $("#bio-placeholder").html(bio_text);
     })
 })
